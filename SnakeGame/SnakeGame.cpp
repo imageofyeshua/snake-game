235e730e8106fb66ec3e5c26f4d3af8e47eaf2c0
Daniel Park <intheimageofyeshua@gmail.com>
2024-09-03 08:12:50 +0900

    Implemented Snake Movement

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <raylib.h>
 #include <deque>
+#include <raymath.h>
 
 using namespace std;
 
@@ -10,19 +11,41 @@ Color darkGreen = { 43, 51, 24, 255 };
 int cellSize = 30;
 int cellCount = 25;
 
+double lastUpdateTime = 0;
+
+bool eventTriggered(double interval)
+{
+	double currentTime = GetTime();
+
+	if (currentTime - lastUpdateTime >= interval)
+	{
+		lastUpdateTime = currentTime;
+		return true;
+	}
+	return false;
+}
+
 class Snake
 {
 public:
 	deque<Vector2> body = { Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9} };
+	Vector2 direction = { 1, 0 };
 
 	void Draw()
 	{
-		for (int i = 0; i < body.size(); i++)
+		for (unsigned int i = 0; i < body.size(); i++)
 		{
-			int x = body[i].x;
-			int y = body[i].y;
-			DrawRectangle(x * cellSize, y * cellSize, cellSize, cellSize, darkGreen);
+			float x = body[i].x;
+			float y = body[i].y;
+			Rectangle segment = Rectangle{ x * cellSize, y * cellSize, (float)cellSize, (float)cellSize };
+			DrawRectangleRounded(segment, 0.5, 6, darkGreen);
 		}
+	}
+
+	void Update()
+	{
+		body.pop_back();
+		body.push_front(Vector2Add(body[0], direction));
 	}
 };
 
@@ -72,6 +95,29 @@ int main()
 	while (WindowShouldClose() == false)
 	{
 		BeginDrawing();
+
+		// Updating
+		if (eventTriggered(0.2))
+		{
+			snake.Update();
+		}
+
+		if (IsKeyPressed(KEY_UP) && snake.direction.y != 1)
+		{
+			snake.direction = { 0, -1 };
+		}
+		if (IsKeyPressed(KEY_DOWN) && snake.direction.y != -1)
+		{
+			snake.direction = { 0, 1 };
+		}
+		if (IsKeyPressed(KEY_LEFT) && snake.direction.x != 1)
+		{
+			snake.direction = { -1, 0 };
+		}
+		if (IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1)
+		{
+			snake.direction = { 1, 0 };
+		}
 
 		// Drawing
 		ClearBackground(green);
